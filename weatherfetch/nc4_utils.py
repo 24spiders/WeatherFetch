6f4aca9b20ee60b473c99d2cd1993a00c4134285
24spiders <88519228+24spiders@users.noreply.github.com>
2025-04-11 16:08:20 -0600

    Update nc4_utils.py

--- weatherfetch/nc4_utils.py
+++ weatherfetch/nc4_utils.py
@@ -54,7 +54,7 @@ def hours_since(hours_since, reference_date_time):
     """
     ref_time = datetime.strptime(reference_date_time, '%Y-%m-%d %H:%M:%S')
     new_time = ref_time + timedelta(hours=hours_since)
-    return (new_time.year, new_time.month, new_time.day), new_time.hour
+    return (new_time.year, new_time.month, new_time.day), new_time.strftime('%H:%M:%S')
 
 
 def load_nc4(netCDF_file_path, variable=None, verbose=True):
@@ -89,11 +89,11 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
 
     # Get latitude and longitude, filter to nearest n_pts
     if 'rlat' in dataset.variables and 'rlon' in dataset.variables:
-        if verbose:
-            print('Detected rotated pole coordinates!')
-            print(dataset.variables['rotated_pole'])
         rotated_pole_lat = dataset.variables['rotated_pole'].grid_north_pole_latitude
         rotated_pole_lon = dataset.variables['rotated_pole'].grid_north_pole_longitude
+        if verbose:
+            print('Detected rotated pole coordinates!')
+            print(f'Rotated Pole Lat: {rotated_pole_lat}, Rotated Pole Lon: {rotated_pole_lon}')
         rlats = dataset.variables['rlat']
         rlons = dataset.variables['rlon']
         # Create meshgrid of rlats and rlons
@@ -112,7 +112,7 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
         lons_flat = lat_grid.flatten()
 
     # Get date
-    time_var = next((var for var in ['Time', 'time'] if var in dataset.variables), None)
+    time_var = next((var for var in ['Time', 'time', 'time1'] if var in dataset.variables), None)
     time_var = dataset.variables[time_var]
 
     # Catch warnings here: dtype compatibility can be noisy due to issues with the netCDF file format
@@ -121,12 +121,13 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
         time_var_data = time_var[:]
 
     time_units = time_var.units
-    date = nc.num2date(time_var_data,
-                       units=time_units,
-                       calendar='standard')[0].strftime('%Y-%m-%d %H:%M:%S')
 
     # If hourly data
     if time_var.shape[0] == 24:
+        # Get date
+        date = nc.num2date(time_var_data,
+                           units=time_units,
+                           calendar='standard')[0].strftime('%Y-%m-%d %H:%M:%S')
         # Create rows for each hour
         var_flat = var_data.reshape(24, -1)  # [24 hours, lat * lon]
 
@@ -154,13 +155,16 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
 
     # Else, daily
     elif time_var.shape[0] == 1:
+        # Get date
+        date = nc.num2date(time_var_data,
+                           units=time_units,
+                           calendar='standard')[0].strftime('%Y-%m-%d %H:%M:%S')
         if 'hours since' in time_var.units:
             ref_date_time = ' '.join(time_var.units.split()[2:])
             _, time = hours_since(time_var_data[0], ref_date_time)
         dates_flat = np.array([date] * len(lons_flat))
         times_flat = np.array([time] * len(lons_flat))
         var_flat = var_data.flatten()
-
         # Create DataFrame
         df = pd.DataFrame({
             'latitude': lats_flat,
@@ -169,6 +173,40 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
             'time': times_flat,
             variable: var_flat
         })
+    # Else, special case
+    elif len(time_var) != 1:
+        df = pd.DataFrame()
+        for i, time in enumerate(time_var):
+            var_data_at_time = var_data[i]
+            date = nc.num2date(time_var_data,
+                               units=time_units,
+                               calendar='standard')[i].strftime('%Y-%m-%d %H:%M:%S')
+            if 'hours since' in time_var.units:
+                ref_date_time = ' '.join(time_var.units.split()[2:])
+                _, time = hours_since(float(time), ref_date_time)
+            dates_flat = np.array([date] * len(lons_flat))
+            times_flat = np.array([time] * len(lons_flat))
+            var_flat = var_data_at_time.flatten()
+            # Create DataFrame
+            if 'rlat' in dataset.variables and 'rlon' in dataset.variables:
+                temp_df = pd.DataFrame({
+                    'rotated_latitude': rlats_flat,
+                    'rotated_longitude': rlons_flat,
+                    'latitude': lats_flat,
+                    'longitude': lons_flat,
+                    'date': dates_flat,
+                    'time': times_flat,
+                    variable: var_flat
+                })
+            else:
+                temp_df = pd.DataFrame({
+                    'latitude': lats_flat,
+                    'longitude': lons_flat,
+                    'date': dates_flat,
+                    'time': times_flat,
+                    variable: var_flat
+                })
+            df = pd.concat([df, temp_df], ignore_index=True)
 
     # Remove rows where variable is NaN or masked
     df = df.dropna()
@@ -179,23 +217,18 @@ def load_nc4(netCDF_file_path, variable=None, verbose=True):
     return df
 
 
-def netcdf_to_csv(netCDF_file_path, variable, output_csv_path, bbox=None, dates=None):
+def filter_df(df, bbox=None, dates=None):
     """
-    Converts a netCDF file to CSV with optional spatial and temporal filtering.
+    Filters a DataFrame with optional spatial and temporal filtering.
 
     Args:
-        netCDF_file_path (str): The path to the netCDF file to load.
-        variable (str): The name of the variable to retrieve from the netCDF file.
-        output_csv_path (str): The path to save the CSV file.
+        df (pd.DataFrame): Pandas dataframe to filter
         bbox (tuple, optional): Bounding box (min_lon, min_lat, max_lon, max_lat) in lat/lon (EPSG:4326).
         dates (tuple, optional): Date range (min_date, max_date) in format 'YYYY-MM-DD'.
 
     Returns:
-        df (pd.DataFrame): The loaded and filtered data with columns ['latitude', 'longitude', 'date', variable].
+        df (pd.DataFrame): The filtered data
     """
-    # Load the netCDF file into a DataFrame
-    df = load_nc4(netCDF_file_path, variable, verbose=False)
-
     # Ensure date column is in datetime format
     try:
         df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y %I:%M:%S %p')
@@ -215,7 +248,33 @@ def netcdf_to_csv(netCDF_file_path, variable, output_csv_path, bbox=None, dates=
         max_date = pd.to_datetime(max_date)
         df = df[(df['date'] >= min_date) & (df['date'] <= max_date)]
 
+    return df
+
+
+def netcdf_to_csv(netCDF_file_path, variable, output_csv_path, bbox=None, dates=None):
+    """
+    Converts a netCDF file to CSV with optional spatial and temporal filtering.
+
+    Args:
+        netCDF_file_path (str): The path to the netCDF file to load.
+        variable (str): The name of the variable to retrieve from the netCDF file.
+        output_csv_path (str): The path to save the CSV file.
+        bbox (tuple, optional): Bounding box (min_lon, min_lat, max_lon, max_lat) in lat/lon (EPSG:4326).
+        dates (tuple, optional): Date range (min_date, max_date) in format 'YYYY-MM-DD'.
+
+    Returns:
+        df (pd.DataFrame): The loaded and filtered data
+    """
+    # Load the netCDF file into a DataFrame
+    df = load_nc4(netCDF_file_path, variable, verbose=False)
+
+    df = filter_df(df, bbox, dates)
+
     # Save the filtered DataFrame to CSV
     df.to_csv(output_csv_path, index=False)
 
     return df
+
+if __name__ == '__main__':
+    file = r'D:\Users\Liam\Documents\01 - University\Research\Python\Piyush\HRDPS Wind\2017082800_008.nc'
+    u_df = load_nc4(file, 'UU')
